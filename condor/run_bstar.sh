9c7550b8e173709be54de7a100c9d7d56fa143df
Lucas Corcodilos <lcorcodi@cern.ch>
2021-01-14 12:01:49 -0600

    Add condor venv fix

--- condor/run_bstar.sh
+++ condor/run_bstar.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 echo "Run script starting"
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-xrdcp root://cmseos.fnal.gov//store/user/lcorcodi/CMSDAS2021env.tgz ./
+xrdcp root://cmsxrootd.fnal.gov//store/user/lcorcodi/CMSDAS2021env.tgz CMSDAS2021env.tgz
 export SCRAM_ARCH=slc7_amd64_gcc820
-tar -xzf CMSDAS2021env.tgz
+tar -xzvf CMSDAS2021env.tgz
 rm CMSDAS2021env.tgz
 
 mkdir tardir; cp tarball.tgz tardir/; cd tardir/
 tar -xzf tarball.tgz; rm tarball.tgz
-cp -r * ../CMSSW_11_0_1/src/BstarToTW_CMSDAS2021; cd ../CMSSW_11_0_1/src/
+cp -r * ../CMSSW_11_0_1/src/BstarToTW_CMSDAS2021/; cd ../CMSSW_11_0_1/src/
 eval `scramv1 runtime -sh`
+rm -rf timber-env
+python -m virtualenv timber-env
 source timber-env/bin/activate
+cd TIMBER
+source setup.sh
 
-cd BstarToTW_CMSDAS2021
+cd ../BstarToTW_CMSDAS2021
 
 echo python bs_select.py $*
 python bs_select.py $*
 
-cp Presel_*.root ../../
+cp Presel_*.root ../../../
