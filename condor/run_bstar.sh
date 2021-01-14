2d33adcfcb23e444f54e8f141b8fcff900ced16c
Lucas Corcodilos <lcorcodi@cern.ch>
2021-01-14 10:18:00 -0600

    More condor fix attempts

--- condor/run_bstar.sh
+++ condor/run_bstar.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 echo "Run script starting"
 source /cvmfs/cms.cern.ch/cmsset_default.sh
+xrdcp root://cmseos.fnal.gov//store/user/lcorcodi/CMSDAS2021env.tgz ./
 export SCRAM_ARCH=slc7_amd64_gcc820
-eval `scramv1 project CMSSW CMSSW_11_0_1`
+tar -xzf CMSDAS2021env.tgz
+rm CMSDAS2021env.tgz
 
 mkdir tardir; cp tarball.tgz tardir/; cd tardir/
 tar -xzf tarball.tgz; rm tarball.tgz
-cp -r * ../CMSSW_11_0_1/src/; cd ../CMSSW_11_0_1/src/
+cp -r * ../CMSSW_11_0_1/src/BstarToTW_CMSDAS2021; cd ../CMSSW_11_0_1/src/
 eval `scramv1 runtime -sh`
+source timber-env/bin/activate
 
-rm rootfiles/*
-rm plots/*
+cd BstarToTW_CMSDAS2021
 
 echo python bs_select.py $*
 python bs_select.py $*
