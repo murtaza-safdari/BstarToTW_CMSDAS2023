beeb58de7a4ab7b5a1b07595628ed7e0add571e8
Amitav Mitra <amitra3@jhu.edu>
2022-12-20 16:34:26 -0600

    update for CMSDAS 2023

--- gencounttest.py
+++
@@ -1,8 +0,0 @@
-from TIMBER.Analyzer import analyzer
-import sys
-
-a = analyzer(sys.argv[1])
-
-print (a.RunChain.genEventCount_)
-# print (a.RunChain.genEventSumw_)
-# print (a.RunChain.genEventSumw2_)
