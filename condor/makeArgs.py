3944000c1744c675899df2b346bc6c8bd598c826
Amitav Mitra <amitra3@jhu.edu>
2022-12-21 16:52:03 -0600

    fix condor submission args

--- condor/makeArgs.py
+++ condor/makeArgs.py
@@ -15,8 +15,8 @@ f18 = open('condor/2018_args.txt','w')
 for f in files.split('\n'):
     if (f == '') or ('prime' in f) or ('scale' in f) or ('ext' in f) or ('jets' in f) or ('DS' in f): continue
     if 'bstar16' in f:
-	f16.write('-s {} -y 16\n'.format(f))
+	f16.write(' -s {} -y 16\n'.format(f))
     elif 'bstar17' in f:
-	f17.write('-s {} -y 17\n'.format(f))
+	f17.write(' -s {} -y 17\n'.format(f))
     else:
-	f18.write('-s {} -y 18\n'.format(f))
+	f18.write(' -s {} -y 18\n'.format(f))
